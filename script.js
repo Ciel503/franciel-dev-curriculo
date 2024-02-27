e77cfcc0ffef5d09cc69c47a72e7f4bb905d5f45
Franciel Mesquita <106025802+Ciel503@users.noreply.github.com>
2024-02-26 22:30:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function inserir(){
+    document.getElementById("rnome").innerText = document.getElementById("nome").value
+    document.getElementById("rcidade").innerText = document.getElementById("cidade").value
+    document.getElementById("ridade").innerText = document.getElementById("idade").value
+    document.getElementById("remail").innerText = document.getElementById("email").value
+    document.getElementById("rtelefone").innerText = document.getElementById("telefone").value
+    document.getElementById("robijetivo").innerText = document.getElementById("objetivo").value
+    document.getElementById("rformacao").innerText = document.getElementById("formacao").value
+    document.getElementById("rcertificados").innerText = document.getElementById("certificados").value
+    document.getElementById("rexperiencia").innerText = document.getElementById("experiencia").value
+    document.getElementById("rhabilidades").innerText = document.getElementById("habilidades").value
+
+
+
+
+
+
+}
